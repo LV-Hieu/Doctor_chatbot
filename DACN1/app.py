f41823668f0aa7b3039be415078696ff34d1de70
Lý Văn Hiếu <hieu.lv240108e@sis.hust.edu.vn>
2024-10-26 23:53:09 +0700

    Add files via upload

--- DACN1/app.py
+++ DACN1/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, jsonify
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from unsloth import FastLanguageModel
+from datasets import load_dataset
+from unsloth import is_bfloat16_supported
+from transformers import AutoTokenizer, TextStreamer
 import torch
 
 app = Flask(__name__)
 
+# Kiểm tra xem CUDA có sẵn không
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # Tải mô hình và tokenizer
-model_name = './health_chatbot_model'  # Đường dẫn đến mô hình đã lưu
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+model_name = './fine_tuned_llama3b'  
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = FastLanguageModel.from_pretrained(model_name)
+
+# Di chuyển mô hình vào thiết bị (CPU hoặc GPU)
+model.to(device)
 
 # Đảm bảo mô hình ở chế độ đánh giá
 model.eval()
@@ -16,6 +26,18 @@ model.eval()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Format
+alpaca_prompt = """Dưới đây là hướng dẫn mô tả một nhiệm vụ. Viết một phản hồi hoàn thành yêu cầu một cách thích hợp.
+
+### Nhiệm vụ:
+{}
+
+### Đầu vào:
+{}
+
+### Câu trả lời:
+{}"""
 
 # Route để xử lý phản hồi từ chatbot
 @app.route('/get_response', methods=['POST'])
@@ -27,9 +49,22 @@ def get_bot_response():
         return jsonify({"response": "Không có nội dung nhập từ người dùng."})
 
     try:
-        # Tạo phản hồi từ mô hình
-        inputs = tokenizer.encode(user_input, return_tensors='pt')  # Mã hóa đầu vào
-        outputs = model.generate(inputs, max_length=100, num_return_sequences=1)  # Tạo đầu ra
+        # Sử dụng Alpaca prompt để tạo phản hồi từ mô hình
+        inputs = tokenizer(
+            [
+                alpaca_prompt.format(
+                    "If you are a doctor, please answer the medical questions based on the patient's description.",  # instruction
+                    user_input,  # input - dùng biến user_input từ đầu vào của người dùng
+                    "",  # output - để trống cho phần tạo văn bản mới
+                )
+            ], 
+            return_tensors="pt"
+        ).to(device)
+
+        # Sử dụng TextStreamer để stream kết quả
+        text_streamer = TextStreamer(tokenizer)
+        outputs = model.generate(**inputs, streamer=text_streamer, max_new_tokens=128*8)
+
         bot_response = tokenizer.decode(outputs[0], skip_special_tokens=True)  # Giải mã đầu ra
 
         return jsonify({"response": bot_response})
